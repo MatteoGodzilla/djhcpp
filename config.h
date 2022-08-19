5b68f58778542fab11a6dada0304d758015fab58
MatteoGodzilla <catena.matteo90@gmail.com>
2022-08-19 13:06:55 +0200

    advanced version to v0.1.8

--- config.h
+++ config.h
@@ -2,6 +2,6 @@
 #include <string>
 
 //will be replaced by cmake with the correct version string;
-const std::string DJHCPP_VERSION = "0.1.7";
+const std::string DJHCPP_VERSION = "0.1.8";
 const std::string DJHCPP_BUILD = "Debug";
 const std::string SETTINGS_FILE_NAME = "djhcpp_settings.ini";
